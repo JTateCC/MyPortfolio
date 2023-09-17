3e2bcef3248843068348445935dc1bf256867ef9
jtate <jtate.cc@outlook.com>
2023-09-17 15:01:57 +0100

    added media root settings andurl

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,11 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.views.generic.base import TemplateView
+
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
     path('blog/', include('blog.urls'))
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
