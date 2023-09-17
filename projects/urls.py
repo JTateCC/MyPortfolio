b00b27dfb3cacdaa3eb6082f297ff011bff54ccf
jtate <jtate.cc@outlook.com>
2023-09-17 15:51:56 +0100

    changng tet db

--- projects/urls.py
+++ projects/urls.py
@@ -5,7 +5,6 @@ app_name = 'projects'
 
 urlpatterns = [
     path('', views.all_projects, name='all_projects'),
-    path('update_server/', views.update_server, name='update'),
     path('<int:pk>', views.project_detail, name='project_detail')
     ]
 
