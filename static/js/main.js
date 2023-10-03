8abc27b9c2448c9771f251b64d0af4bded57ad0f
jtate <jtate.cc@outlook.com>
2023-10-03 15:23:02 +0100

    finalised styling of front page

--- static/js/main.js
+++
@@ -1,9 +0,0 @@
-
-const contactBtn = document.getElementById("contact-btn");
-contactBtn.addEventListener("click",() => {
-    console.log("triggered")
-    $("#about-me-div").slideToggle();
-    aboutMeDiv = document.getElementById("about-me-div");
-    aboutMeDiv.style.display = 'block'
-    aboutMeDiv.style.position= 'relative'
-})
